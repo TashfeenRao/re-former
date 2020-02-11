664d7654755a182f32bb428c66b89f5de55876c5
konyan <devkonyan@gmail.com>
2020-02-12 03:52:28 +0630

    add user create method at controller and redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new()
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #success
+      flash[:success] = "User Registered on System!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user)
+            .permit(:username,:email,:password,:password_confirmation)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get '/users/new', to:'users#new'
+  # post '/users/create',  to: 'users#create'
+
   resources :users
 end
