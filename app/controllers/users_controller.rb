fa7169fec3795a07d4b0785e785ae7cbde0ed7d1
TashfeenRao <raotashfeen25@gmail.com>
2020-02-12 02:50:17 +0500

    added edit action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,19 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
-
+  def edit
+    @user = User.find(params[:id])
+    
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "User Updated on System!"
+      redirect_to user_url(@user)
+    else
+      render 'edit'
+    end
+  end
   private
     def user_params
       params.require(:user)
--- db/schema.rb
+++ db/schema.rb
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_02_11_201619) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
