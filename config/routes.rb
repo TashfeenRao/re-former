a808962a0ab8868102254e004b502bdb05c2afae
TashfeenRao <raotashfeen25@gmail.com>
2020-02-12 23:16:09 +0500

    remove linter errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/signup', to:'users#new'
-  post '/signup',  to: 'users#create'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  get '/edit', to:'users#edit'
-  patch '/edit',  to: 'users#update'
+  get '/edit', to: 'users#edit'
+  patch '/edit', to: 'users#update'
 
-  resources :users, only: [:new, :create, :edit, :update]
+  resources :users, only: %i[new create edit update]
 end
